05ddd50ce0e02e845b1cee5c24b6ed0cc5004861
bqdong <bqdong.dev@gmail.com>
2024-09-10 22:51:40 +0800

    add test

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 
+#[derive(Debug)]
 pub enum JsonValue {
     Null,
     Bool(bool),
@@ -12,15 +13,41 @@ pub enum JsonValue {
 pub type JsonObject = HashMap<String, JsonValue>;
 pub type JsonArray = Vec<JsonValue>;
 
+#[derive(Debug)]
 pub enum Json {
     Object(JsonObject),
     Array(JsonArray),
 }
 
-pub fn stringify(json: Json) -> String {
+pub fn stringify<T>(data: T) -> String
+where
+    T: Into<Json>,
+{
+    let json: Json = data.into();
+    match json {
+        Json::Object(object) => {
+            println!("object: {:?}", object);
+        }
+        Json::Array(array) => {
+            println!("array: {:?}", array);
+        }
+    }
     unimplemented!()
 }
 
-pub fn parse(json: &str) -> Json {
+pub fn parse(_json: &str) -> Json {
     unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_stringify() {
+        let json = Json::Array(Vec::new());
+        let result = stringify(json);
+        assert_eq!(result, String::from("[]"));
+    }
 }
