9719fb6f30703938db1251870886ece139f82906
bqdong <bqdong.dev@gmail.com>
2024-09-11 22:48:37 +0800

    stringify

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::fmt;
 
 #[derive(Debug)]
 pub enum JsonValue {
@@ -8,6 +9,37 @@ pub enum JsonValue {
     String(String),
     Array(Vec<JsonValue>),
     Object(HashMap<String, JsonValue>),
+}
+
+impl fmt::Display for JsonValue {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            JsonValue::Null => write!(f, "null"),
+            JsonValue::Bool(b) => write!(f, "{}", b),
+            JsonValue::Number(n) => write!(f, "{}", n),
+            JsonValue::String(s) => write!(f, "\"{}\"", s),
+            JsonValue::Array(a) => {
+                write!(f, "[")?;
+                for (i, item) in a.iter().enumerate() {
+                    if i > 0 {
+                        write!(f, ", ")?;
+                    }
+                    write!(f, "{}", item.to_string())?;
+                }
+                write!(f, "]")
+            }
+            JsonValue::Object(o) => {
+                write!(f, "{{")?;
+                for (i, (key, value)) in o.iter().enumerate() {
+                    if i > 0 {
+                        write!(f, ", ")?;
+                    }
+                    write!(f, "\"{}\": {}", key, value.to_string())?;
+                }
+                write!(f, "}}")
+            }
+        }
+    }
 }
 
 pub type JsonObject = HashMap<String, JsonValue>;
@@ -25,14 +57,9 @@ where
 {
     let json: Json = data.into();
     match json {
-        Json::Object(object) => {
-            println!("object: {:?}", object);
-        }
-        Json::Array(array) => {
-            println!("array: {:?}", array);
-        }
+        Json::Object(object) => JsonValue::Object(object).to_string(),
+        Json::Array(array) => JsonValue::Array(array).to_string(),
     }
-    unimplemented!()
 }
 
 pub fn parse(_json: &str) -> Json {
@@ -49,5 +76,15 @@ mod tests {
         let json = Json::Array(Vec::new());
         let result = stringify(json);
         assert_eq!(result, String::from("[]"));
+    }
+
+    #[test]
+    fn test_stringify_object() {
+        let json = Json::Object(HashMap::from([
+            ("key1".to_string(), JsonValue::String("value1".to_string())),
+            ("key2".to_string(), JsonValue::Number(42f64)),
+        ]));
+        let result = stringify(json);
+        assert_eq!(result, String::from("{\"key1\": \"value1\", \"key2\": 42}"));
     }
 }
